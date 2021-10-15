1b36f352f1d0519c662fcdcf7a9cf153857bdc05
Nathan Elliott <nelliott13@users.noreply.github.com>
2021-10-15 14:35:21 -0500

    Add function to parse project filenames

--- code/00_definitions.R
+++ code/00_definitions.R
@@ -7,7 +7,6 @@
 # Wrapper for base 'message' function
 message_ts <- function(...) message("[", Sys.time(), "] - ", ...)
 
-
 # Set directories ---------------------------------------------
 
 base_dir <- getwd() #replace as necessary with "YOUR/BASE/DIR"
@@ -20,6 +19,8 @@ wtr_dir <- file.path(data_dir, "water")
 avg_dir <- file.path(data_dir, "water_averages")
 cov_dir <- file.path(data_dir, "other_covariates")
 
+wfc_dir <- file.path(data_dir, "water_forecast")
+wxl_dir <- file.path(data_dir, "water_x_landcover")
 
 # Check that specified directories exist, optionally creating if missing
 check_dir <- function(directory, create = FALSE, verbose = FALSE) {
@@ -51,14 +52,55 @@ check_dir <- function(directory, create = FALSE, verbose = FALSE) {
 }
 check_dir(base_dir)
 if(file.exists(base_dir)) {
-  check_dir(c(code_dir, data_dir, lc_dir, run_dir, pcp_dir, wtr_dir, avg_dir, cov_dir), create = TRUE)
+  check_dir(c(code_dir, data_dir, lc_dir, run_dir, pcp_dir, wtr_dir, avg_dir, cov_dir, wfc_dir, wxl_dir), create = TRUE)
 }
 
 # Check that code_dir actually contains the necessary code
 
 
 # Shared Functions --------------------
+# Function to get the nth subelement from each item in a list
+extract_subelement <- function(x, element) sapply(x, `[[`, element) 
 
+# Function to parse the filenames of project files
+parse_filename <- function(files) {
+  
+  paths <- dirname(files)
+  fns <- basename(files)
+  
+  # Extension
+  ext_pos <- regexpr("\\.([[:alnum:]]+)$", fns)
+  exts <- ifelse(ext_pos > -1L, substring(fns, ext_pos + 1L), "")
+  fns_no_ext <- ifelse(ext_pos > -1L, substring(fns, 1, ext_pos - 1L), fns)
+  
+  # Order of data in filenames is location, descriptor, year, month, additional info
+  fns_split <- strsplit(fns_no_ext, "_")
+  locs <- extract_subelement(fns_split, 1)
+  desc <- extract_subelement(fns_split, 2)
+  yrs <- extract_subelement(fns_split, 3)
+  mths <- extract_subelement(fns_split, 4)
+  
+  # Amount of additional info not set; could be none or could be 3-4
+  n_splits <- length(fns_split[[1]])
+  info <- paste(ifelse(n_splits > 4, extract_subelement(fns, 5), ""),
+                ifelse(n_splits > 5, extract_subelement(fns, 6), ""),
+                ifelse(n_splits > 6, extract_subelement(fns, 7), ""),
+                sep = "_")
+  
+  # Combine into data frame and export
+  file_df <- data.frame("File" = files,
+                        "Path" = paths, 
+                        "Filename" = fns,
+                        "Extension" = exts,
+                        "Location" = locs,
+                        "Description" = desc,
+                        "Year" = yrs,
+                        "Month" = mths,
+                        "AdditionalInfo" = info)
+  
+  return(file_df)
+  
+}
 
 
 
