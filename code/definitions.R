dc0fe1e81eadad886cf67c88c974fc38d4c28b09
Nathan Elliott <nelliott13@users.noreply.github.com>
2022-01-18 10:34:25 -0600

    Split definitions file into definitions and functions
    Definitions of directories and other parameters now in definitions.R;
    
    All functions moved to 00_shared_functions.R

---
+++ code/definitions.R
@@ -0,0 +1,65 @@
+# Defines project directories and input file characteristics
+#  
+# Point Blue, California Rice Commission
+
+# Set directories ---------------------------------------------
+base_dir <- getwd() #replace as necessary with "YOUR/BASE/DIR"
+code_dir <- file.path(base_dir, "code")
+data_dir <- file.path(base_dir, "data")
+lc_dir <- file.path(data_dir, "landcover")
+run_dir <- file.path(data_dir, "runoff")
+pcp_dir <- file.path(data_dir, "precip")
+wtr_dir <- file.path(data_dir, "water")
+avg_dir <- file.path(data_dir, "water_average")
+wfc_dir <- file.path(data_dir, "water_forecast")
+cov_dir <- file.path(data_dir, "other_covariates")
+
+mdl_dir <- file.path(data_dir, "models")
+brd_mdl_dir <- file.path(mdl_dir, "birds")
+
+axn_dir <- file.path(data_dir, "example")
+fld_dir <- file.path(axn_dir, "fields")
+
+# Average flooding (auction-level)
+scn_avg_dir <- file.path(axn_dir, "scenario_average_water")
+avg_wxl_dir <- file.path(scn_avg_dir, "water_x_landcover")
+avg_fcl_dir <- file.path(scn_avg_dir, "water_focal")
+avg_prd_dir <- file.path(scn_avg_dir, "bird_predictions")
+avg_stat_dir <- file.path(scn_avg_dir, "stats")
+
+# Imposed flooding (field or bid-level)
+scn_imp_dir <- file.path(axn_dir, "scenario_imposed_water")
+imp_imp_dir <- file.path(scn_imp_dir, "water_imposed")
+imp_wxl_dir <- file.path(scn_imp_dir, "water_x_landcover")
+imp_fcl_dir <- file.path(scn_imp_dir, "water_focal")
+imp_prd_dir <- file.path(scn_imp_dir, "bird_predictions")
+imp_stat_dir <- file.path(scn_imp_dir, "stats")
+
+# Bird definitions
+bird_df <- data.frame("CommonName" = c("American Avocet", "Black-necked Stilt", "Dowitcher", "Dunlin", 
+                                       "Northern Pintail", "Northern Shoveler", "Green-winged Teal"),
+                      "CommonCode" = c("AMAV", "BNST", "DOWI", "DUNL", "NOPI", "NSHO", "GWTE"),
+                      "ScientificCode" =c("REAM", "HIME", "LISPP", "CALA", "ANAC", "ANCL", "ANCR"))
+
+# Bird models
+shorebird_sci_base <- paste(rep(c("CALA", "HIME", "LISPP", "REAM"), each = 2), c("N", "S"), sep = "_")
+shorebird_com_base <- paste(rep(c("DUNL", "BNST", "DOWI", "AMAV"), each = 2), c("N", "S"), sep = "_")
+# Two models for each species and model type, one for the north valley one and for the south
+# Ensemble at (2N + 1S) / 3 for north and (1N + 2S) / 3 for south
+# Reallong models that combine long-term and real-time water data
+shorebird_model_files_reallong <- file.path(brd_mdl_dir, paste0(shorebird_sci_base, "_Reallong_nopattern_subset.rds"))
+shorebird_model_names_reallong <- paste0(shorebird_com_base, "_reallong")
+
+# Long models that just use the long-term average
+shorebird_model_files_long <- file.path(brd_mdl_dir, paste0(shorebird_sci_base, "_Long_lowN_subset.rds"))
+shorebird_model_names_long <-  paste0(shorebird_com_base, "_longterm")
+
+# Static covariates
+bird_model_cov_files <- file.path(cov_dir, c("data_type_constant_ebird_p44r33.tif", "valley_roads_p44r33.tif"))
+bird_model_cov_names <- c("COUNT_TYPE2", "roads5km")
+
+# Monthly covariates (tmax)
+mths <- month.abb[c(1:4, 7:12)]
+tmax_files <- file.path(cov_dir, paste0("tmax_", mths, "_p44r33.tif"))
+tmax_months <- mths
+tmax_names <- rep("tmax250m", length(mths))
---
+++ code/functions/00_shared_functions.R
@@ -0,0 +1,142 @@
+# Low-level functions for water tracker project, including logging and filename parsing
+#  
+# Point Blue, California Rice Commission
+
+# Shared Functions --------------------
+
+# Basic logging function to add a timestamp to strings
+add_ts <- function(...) paste0("[", Sys.time(), "] - ", ...)
+
+# Add as wrapper for base 'message' function
+message_ts <- function(...) message(add_ts(...))
+
+# Function to check directories exist, optionally loading
+check_dir <- function(directory, create = FALSE, verbose = FALSE) {
+  
+  for (d in directory) {
+    
+    if (!file.exists(d)) {
+      
+      if (create == FALSE) {
+        
+        message_ts()
+        stop(paste("Directory", d, "is required but does not exist."))
+        
+      } else {
+        
+        message("Creating directory ", d)
+        dir.create(d)
+        
+      }
+      
+    } else if (verbose == TRUE) {
+      
+      message("Directory", d, "exists.")
+      
+    } 
+    
+  }
+  
+}
+
+# Function to get the nth subelement from each item in a list
+extract_subelement <- function(x, element) sapply(x, `[[`, element) 
+
+# Function to make a string filename safe
+clean_string <- function(x, sub_char = "-", ...) {
+  gsub("[^a-zA-Z0-9_\\-]", sub_char, x, ...)
+}
+
+# Function to make a string filename safe
+clean_string_remove_underscores <- function(x, sub_char = "-", ...) {
+  gsub("[^a-zA-Z0-9\\-]", sub_char, x, ...)
+}
+
+# Function to more quickly trim rasters
+# raster::trim is ridiculously slow
+trim_faster <- function(x, out = "raster"){
+  
+  if(!require(raster)) stop(add_ts("Package 'raster' is required"))
+  
+  # Check inputs
+  if(!(class(x) %in% c("RasterLayer", "matrix"))) stop("Input must be a raster or matrix")
+  if(!(out %in% c("raster", "matrix"))) stop("Output must be a raster or matrix")
+  if(class(x) == "matrix" & out == "raster") stop("if you supply a matrix, you must use out='matrix'")
+  
+  # Convert to matrix
+  if(class(x) == "RasterLayer") {
+    if(out == "raster") { 
+      cres <- 0.5 * res(x)
+      crs <- projection(x)
+      ref_rst <- x 
+    }
+    x <- matrix(raster::as.array(x), nrow = nrow(x), ncol = ncol(x))
+  }
+  
+  # Check rows and columns for NAs
+  na_rows <- apply(x, MARGIN = 1, FUN = function(x) { all(is.na(x)) })
+  na_cols <- apply(x, MARGIN = 2, FUN = function(x) { all(is.na(x)) })
+  
+  # Find first/last non-NA rows and columns
+  r1 <- min(which(!na_rows))
+  r2 <- max(which(!na_rows))
+  c1 <- min(which(!na_cols))
+  c2 <- max(which(!na_cols))
+  
+  # Subset matrix
+  x <- x[r1:r2,c1:c2]
+  
+  # Reformat as raster
+  if (out == "raster") {
+    xs <- xFromCol(ref_rst, col = c(c1, c2)) + c(-1, 1) * cres[1]
+    ys <- yFromRow(ref_rst, row = c(r2, r1)) + c(-1, 1) * cres[2]
+    x <- raster(x, xmn = xs[1], xmx = xs[2], ymn = ys[1], ymx = ys[2], crs = crs)
+  }
+  
+  return(x)
+  
+}
+
+
+# Function to parse the filenames of project files
+parse_filename <- function(files) {
+  
+  paths <- dirname(files)
+  fns <- basename(files)
+  
+  # Extension
+  ext_pos <- regexpr("\\.([[:alnum:]]+)$", fns)
+  exts <- ifelse(ext_pos > -1L, substring(fns, ext_pos + 1L), "")
+  fns_no_ext <- ifelse(ext_pos > -1L, substring(fns, 1, ext_pos - 1L), fns)
+  
+  # Order of data in filenames is location, descriptor, year, month, additional info
+  fns_split <- strsplit(fns_no_ext, "_")
+  locs <- extract_subelement(fns_split, 1)
+  desc <- extract_subelement(fns_split, 2)
+  yrs <- extract_subelement(fns_split, 3)
+  mths <- extract_subelement(fns_split, 4)
+  
+  # Amount of additional info not set; could be none or could be 3-4
+  n_splits <- length(fns_split[[1]])
+  info <- paste(ifelse(n_splits > 4, extract_subelement(fns_split, 5), ""),
+                ifelse(n_splits > 5, extract_subelement(fns_split, 6), ""),
+                ifelse(n_splits > 6, extract_subelement(fns_split, 7), ""),
+                sep = "_")
+  
+  # Combine into data frame and export
+  file_df <- data.frame("File" = files,
+                        "Path" = paths, 
+                        "Filename" = fns,
+                        "Extension" = exts,
+                        "Location" = locs,
+                        "Description" = desc,
+                        "Year" = yrs,
+                        "Month" = mths,
+                        "AdditionalInfo" = info)
+  
+  return(file_df)
+  
+}
+
+
+
